58c02096ebdcf6760e7603ca8c88806d137e0268
arnabera <86921719+arnabera@users.noreply.github.com>
2023-09-16 15:11:00 +0000

    datatype completed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+// basically there are 8 datatypes in js
+// 1. number
+    let age = 26
+    console.log(typeof age);
+// 2. bigInt 
+
+// 3. boolean 
+    let isLoggedIn = true
+    console.log(typeof isLoggedIn);
+// 4. string 
+    let name = "Arnab"
+    console.log(typeof name)
+// 5. null 
+    let password = null
+    console.log(typeof password)
+// 6. undefined 
+    let city ;
+    console.log(typeof city);
+// 7. object 
+    console.log(typeof null); // type of null is always boject
+// 8. symbol
