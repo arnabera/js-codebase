ded88742e90c78482b127288182c6f2e1be1ca34
arnabera <86921719+arnabera@users.noreply.github.com>
2023-09-16 15:34:49 +0000

    type conversion basics part 1 done

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,23 @@
+let age = "24abc"
+
+let umr = Number(age)
+// console.log(typeof age)
+// console.log(typeof umr);
+// console.log(umr)
+
+// "33" => value 
+// "33abc" => NaN ; null => 0
+// true => 1 ; undefined => NaN
+
+
+let isAudult = 1
+let isAudultInBoolean = Boolean(isAudult)
+console.log(isAudultInBoolean);
+
+// 1=> true ; 0 => false 
+// "" => false ; "arnab" => true
+
+
+let price = 3300
+let stringPrice = String(price)
+console.log(stringPrice);
