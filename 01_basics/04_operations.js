d2525169c270aad116438992e0ff0e239218a133
arnabera <86921719+arnabera@users.noreply.github.com>
2023-09-17 05:17:03 +0000

    basic operations are done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,32 @@
+// simple arithmetic operation
+/*
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3**2)
+console.log(3/2)
+console.log(3%2)
+*/
+
+// String operations 
+/*
+let str1 = "Hello"
+let str2 = " arnab"
+let str3 = str1+str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1+ "2");
+console.log(1+"2"+2);
+console.log(1+2+"2");
+console.log("1"+2+2);
+*/
+
+console.log(+true);
+console.log(+"");
+
+// console.log(true+);
+
+let num = 100;
+console.log(num++)
+console.log(++num);
