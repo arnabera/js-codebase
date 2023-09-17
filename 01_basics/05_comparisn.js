27e3c7a2e1219151ebc54fa48baf3ea20948ff9f
arnabera <86921719+arnabera@users.noreply.github.com>
2023-09-17 06:29:01 +0000

    comparison completed

---
+++ 01_basics/05_comparisn.js
@@ -0,0 +1,56 @@
+// comparison of same data type 
+/*
+console.log(1==1);
+console.log(2>1);
+console.log(3<=1);
+console.log("a"=="b");
+*/
+
+
+// comparison of different data typses 
+// console.log(null==true);
+// console.log(null>true);
+// console.log(null<true);
+
+// console.log(null==1)
+// console.log(null <= 1);
+// console.log(null >= 1);
+
+// console.log(null == "");
+// console.log(null <"");
+// console.log(null > "")
+// console.log(null <="");
+// console.log(null >= "")
+
+// console.log(undefined == null);
+// console.log(undefined == 0);
+// console.log(undefined > null);
+// console.log(undefined < null);
+// console.log(undefined == "");
+// console.log(undefined < "");
+// console.log(undefined > "");
+
+
+// console.log("02"==2);
+// console.log("02"===2);
+
+
+let bigNumber = 23633n
+console.log(typeof bigNumber);
+
+let greeting = function(){
+    console.log("Hiiiii ! arnab bera.")
+}
+
+console.log(typeof greeting);
+
+let prices = {
+    "laptop" : 30000,
+    "soap" : 15
+}
+
+console.log(typeof prices);
+
+let names = ["arnab" , "sukumar" , "aparna"]
+console.log(typeof names);
+
