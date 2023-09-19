8c75673e91aa6bcc28360600524cf8eb629c7136
arnabera <86921719+arnabera@users.noreply.github.com>
2023-09-19 14:54:17 +0000

    array function part 1 completed

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,2 +1,27 @@
-let myArray = [0,1,2,3,4,5]
-console.log(typeof myArray);
+let myArray = [0,1,2,3,4,5,8,9]
+// console.log(typeof myArray);
+
+let heros = ["shaktiman","naagraj","krish"]
+
+// console.log(heros.length);
+// heros.push("chota bheem")
+// console.log(heros);
+// heros.pop()
+// console.log(heros);
+
+heros.unshift("ben10")
+// console.log(heros);
+// heros.shift();
+// console.log(heros);
+
+
+console.log('A',myArray);
+console.log(myArray.slice(1,5))
+console.log('B',myArray);
+console.log(myArray.splice(1,5))
+console.log('C',myArray);
+
+// slice function does not make any changes in the array but splice function changes the array
+
+
+
