d7c2570e8e1d2d5cae0b5d613ab4e3c284479732
arnabera <86921719+arnabera@users.noreply.github.com>
2023-09-19 05:45:29 +0000

    string completed

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,4 +1,31 @@
-let name = "arnab"
+let name = "arnab" // declaration of string 
 let repo = 50
 
-console.log(name + " and repo count is "+repo);
+// console.log("your name is "+name + " and repo count is "+repo); // this is not a good practice 
+// usually we use string interpollation
+
+// console.log(`Your name is ${name} and repo count is ${repo}`);
+
+// we can also declare string as object
+let naam = new String("ArnabBera")
+// console.log(naam.__proto__);
+
+// ########### string methos ########
+
+// console.log(naam.length);
+// console.log(naam.toLocaleUpperCase())
+// console.log(naam.indexOf('b'))
+// console.log(naam.charAt(2));
+
+let village = "   dewanchakchoka   "
+// console.log(village.trim());
+let newString = naam.substring(0,8)
+console.log(newString);
+
+let otherString = naam.slice(-2,9)
+console.log(otherString);
+console.log(village.replace('choka','chowka').trim());
+
+let biodata = "Arnab Bera Dewanchak Chowka Ghatal PaschimMedinipur"
+console.log(biodata.includes('Chowka'));
+console.log(biodata.split(" "));
