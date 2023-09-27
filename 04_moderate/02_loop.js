71870448f4cff76029bbd06b7ebdde89fd15ba14
arnabera <86921719+arnabera@users.noreply.github.com>
2023-09-27 04:28:46 +0000

    for and while loop completed

--- 04_moderate/02_loop.js
+++ 04_moderate/02_loop.js
@@ -5,16 +5,74 @@ for (let i = 1; i <= num; i++) {
 }
 
 for(let i = 1;i<5;i++){//outer loop
-    console.log(`outer loop ${i}`);
+    // console.log(`outer loop ${i}`);
     for(let j=0;j<=i;j++){ // inner loop
-        console.log(`${i} : ${j}`);
+        // console.log(`${i} : ${j}`);
     }
 }
 
+for(let i=1;i<=10;i++){
+    // console.log(`\nTable of ${i}\n`);
+    for(let j=1;j<=10;j++){
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
+
+
+let heros = ["batman","superman","ironman","hulk","thor"]
+for(let i=0;i<heros.length;i++){
+    let element = heros[i]
+    // console.log(element);
+}
+
+
 // ++++++++ forEach Loop ++++++
-const array = [1,2,3,4,5,6]
-array.forEach(element => {
+
+heros.forEach(element => {
     // console.log(element);
 });
+
+//++++++ break and continue++++++++
+for(let i=1;i<=10;i++){
+    if(i===5){
+        // console.log(`5 value detected`);
+        break;
+    }
+    // console.log(`the index value is ${i}`);
+}
+
+// console.log(`\n\n`);
+for(let i=1;i<=10;i++){
+    if(i===5){
+        // console.log("5 is the best value");
+        continue;
+    }
+    // console.log(`the index value is ${i}`);
+}
+
+//+++++++++++ while and do while loop ++++++++++++
+let i=1
+while(i<=10){
+    // console.log(i);
+    i++
+}
+
+let j =10
+while(j>=1){
+    // console.log(j)
+    j--
+}
+
+let m = 0
+while(m<heros.length){
+    // console.log(heros[m]);
+    m++
+}
+
+let k =10
+do{
+    console.log(k);
+    k++
+}while(k<10)// condition is always false but at least one time loop will be executed 
 
 
